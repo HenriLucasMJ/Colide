ed6f5cc066652d4e055885a3c1f87b671fba9eb0
unknown <2jd@e041edu.local>
2026-05-12 11:33:42 -0300

    primeiro commit unity

---
+++ Assets/scripts/Movimento.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+
+    private Rigidbody2D myRigidbody;
+    private float horizontal;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+        myRigidbody = GetComponent<Rigidbody2D>();  
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        Debug.Log(horizontal);
+    }
+}
